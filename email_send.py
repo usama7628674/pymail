aeea7a43bec6fb7577fec2af6ed6486acfb46977
Usama Abdul Sattar <usama123456789@protonmail.com>
2018-07-17 19:49:48 +0500

    updated email_send.py

--- email_send.py
+++ email_send.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/python
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -7,6 +7,7 @@ from email import encoders
 import getpass
 from os import system
 import time
+import sys
   
 class colors:
 	OKGREEN = '\033[92m'
@@ -22,15 +23,17 @@ print colors.BLUE + 'To send an email you must have less secure apps enabled in 
  
  
 msg['From'] = raw_input (colors.OKGREEN + '\nYour Email: ')  
+
 user = msg['From']
+
 passwd = getpass.getpass(colors.OKGREEN + 'Password: ') 
 
-print '\nTo send an email to multiple receipents use comma(,) followed by mail address' 
-msg['To'] = raw_input (colors.OKGREEN + 'To: ')
+print colors.BLUE + '\nTo send an email to multiple receipents use comma(,) followed by mail address'
+
+msg['To'] = raw_input (colors.OKGREEN + '\nTo: ')
+
 to = msg['TO'].split(",")
  
-
-
 subject = raw_input (colors.OKGREEN + 'Subject: ')
  
 
@@ -38,55 +41,41 @@ body = raw_input (colors.OKGREEN + 'Message: ')
 
 message = 'From: ' + user + '\nSubject: ' + subject + '\n' + body
 
-
 msg.attach(MIMEText(message, 'plain'))
 
- 
- 
-
-filename = raw_input (colors.OKGREEN + 'Name of file with extension: ')
-try:
+check = raw_input (colors.OKGREEN + "Do you want to attach files [y/n] ")
+if check == 'y':	
+	
 		
-		path = 'attachments/' + filename
-                attachment = open(path, "rb")
-                p = MIMEBase('application', 'octet-stream')
+		system('python -m zipfile -c attachment attachments/*')                 
+		attachment = open('attachment', "rb")
+                p = MIMEBase('application', 'zip')
                 p.set_payload((attachment).read())
                 encoders.encode_base64(p)
-                p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+                p.add_header('Content-Disposition', "attachment; filename= attachment")
  		msg.attach(p)
-		
 
-except:
-                
-		print 'The email will be sent without any attachments'
-                
-	
-
-
-
- 
-
- 
-
+elif check == 'n':		
+		             
+		print 'This email will be sent without any attachment'
+else:
+	print 'Wrong selection'
+	sys.exit()
+        	
 s = smtplib.SMTP('smtp.gmail.com', 587)
  
-
 s.starttls()
  
-
 s.login(user, passwd)
  
-
 text = msg.as_string()
 
-
- 
-# sending the mail
 nomes = input(colors.OKGREEN + 'No. of email(s) to send: ')
 no = 0 	
 while no != nomes:
 	s.sendmail(user, to, text)
-	print colors.OKGREEN + 'sent successfully ' + str(no+1) + ' emails'
+	print colors.BLUE + 'successfully sent ' + str(no+1) + ' emails'
 	no += 1
 	time.sleep(.8)
-s.quit()
+system('rm attachment 2> /dev/null')
+s.quit()			
